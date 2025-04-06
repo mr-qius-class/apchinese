706a2d0b36a7cb6c2a334731e7e45958f687dc9a
bqiu86 <bqiu86@gmail.com>
2025-04-06 13:42:04 -0400

    changed branch.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/apchinese/',
+  deploymentBranch: 'main', 
   trailingSlash: true,
 
   // GitHub pages deployment config.
