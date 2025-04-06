20f7fed969e40cb79df76a2ca4e7f92d08009050
bqiu86 <bqiu86@gmail.com>
2025-04-06 13:26:32 -0400

    updated url for the docusaurus site

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://mr-qius-class.github.io/apchinese/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
